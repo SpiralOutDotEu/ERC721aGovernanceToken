843f873519f74ed7bb5aaad8a57595a0e584b833
Jumaru <spiralout.eu@gmail.com>
2023-11-06 16:41:00 +0200

    ci: :construction_worker: Add CI Release system

---
+++ release.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  branches: ["master"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    ["@semantic-release/exec", {
+      "prepareCmd": "npm version ${nextRelease.version}"
+    }],
+    ["@semantic-release/changelog", {
+      "changelogFile": "CHANGELOG.md",
+    }],
+    ["@semantic-release/git", {
+      "assets": ["CHANGELOG.md", "package.json", "package-lock.json"],
+      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    }],
+  ]
+}
