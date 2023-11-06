7aeb5b90e14410eae7350995f5093f5333276b52
Jumaru <spiralout.eu@gmail.com>
2023-11-06 17:01:12 +0200

    build: :rotating_light: Lint fixes

--- contracts/ERC721aGovernanceToken.sol
+++ contracts/ERC721aGovernanceToken.sol
@@ -7,10 +7,11 @@ import "@openzeppelin/contracts/governance/utils/IVotes.sol";
 
 // Abstract contract that other contracts can inherit to create ERC721a tokens with governance features
 abstract contract ERC721aGovernanceToken is ERC721A, EIP712, Votes {
-    constructor(string memory name_, string memory symbol_, string memory version_)
-        ERC721A(name_, symbol_)
-        EIP712(name_, version_)
-    {}
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        string memory version_
+    ) ERC721A(name_, symbol_) EIP712(name_, version_) {}
 
     // The following functions can be overridden by inheriting contracts as needed
 
@@ -19,11 +20,12 @@ abstract contract ERC721aGovernanceToken is ERC721A, EIP712, Votes {
      * @dev Instead of {_afterTokenTransfer} that is used in ERC721, ERC721A uses {_afterTokenTransfers} (with an 's')
      * Emits a {IVotes-DelegateVotesChanged} event.
      */
-    function _afterTokenTransfers(address from, address to, uint256 firstTokenId, uint256 batchSize)
-        internal
-        virtual
-        override
-    {
+    function _afterTokenTransfers(
+        address from,
+        address to,
+        uint256 firstTokenId,
+        uint256 batchSize
+    ) internal virtual override {
         _transferVotingUnits(from, to, batchSize);
         super._afterTokenTransfers(from, to, firstTokenId, batchSize);
     }
@@ -33,18 +35,24 @@ abstract contract ERC721aGovernanceToken is ERC721A, EIP712, Votes {
      *
      * WARNING: Overriding this function will likely result in incorrect vote tracking.
      */
-    function _getVotingUnits(address account) internal view virtual override returns (uint256) {
+    function _getVotingUnits(
+        address account
+    ) internal view virtual override returns (uint256) {
         return balanceOf(account);
     }
 
-    /* 
-    * @dev ERC721a Governance Token Interface Support
-    * @dev Implements the interface support check for ERC721a Governance Token
-    * @notice Checks if the contract implements an interface you query for, including ERC721A and Votes interfaces
-    * @param interfaceId The interface identifier, as specified in ERC-165
-    * @return True if the contract implements `interfaceId` or if `interfaceId` is the ERC-165 interface
-    */
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721A) returns (bool) {
-        return interfaceId == type(IVotes).interfaceId || super.supportsInterface(interfaceId);
+    /*
+     * @dev ERC721a Governance Token Interface Support
+     * @dev Implements the interface support check for ERC721a Governance Token
+     * @notice Checks if the contract implements an interface you query for, including ERC721A and Votes interfaces
+     * @param interfaceId The interface identifier, as specified in ERC-165
+     * @return True if the contract implements `interfaceId` or if `interfaceId` is the ERC-165 interface
+     */
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view virtual override(ERC721A) returns (bool) {
+        return
+            interfaceId == type(IVotes).interfaceId ||
+            super.supportsInterface(interfaceId);
     }
 }
--- contracts/mocks/TestableERC721aGovernanceToken.sol
+++ contracts/mocks/TestableERC721aGovernanceToken.sol
@@ -4,9 +4,11 @@ pragma solidity ^0.8.20;
 import "../ERC721aGovernanceToken.sol";
 
 contract TestableERC721aGovernanceToken is ERC721aGovernanceToken {
-    constructor(string memory name, string memory symbol, string memory version)
-        ERC721aGovernanceToken(name, symbol, version)
-    {}
+    constructor(
+        string memory name,
+        string memory symbol,
+        string memory version
+    ) ERC721aGovernanceToken(name, symbol, version) {}
 
     function mint(address to, uint256 quantity) public {
         _mint(to, quantity);
