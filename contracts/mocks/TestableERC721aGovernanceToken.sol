0dd443b4f8b39724ebd98eaffa07fc2b1a7c6e59
Jumaru <spiralout.eu@gmail.com>
2023-11-06 16:29:12 +0200

    test: :test_tube: Add a testable mock implementation

---
+++ contracts/mocks/TestableERC721aGovernanceToken.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "../ERC721aGovernanceToken.sol";
+
+contract TestableERC721aGovernanceToken is ERC721aGovernanceToken {
+    constructor(string memory name, string memory symbol, string memory version)
+        ERC721aGovernanceToken(name, symbol, version)
+    {}
+
+    function mint(address to, uint256 quantity) public {
+        _mint(to, quantity);
+    }
+
+    function burn(uint256 tokenId) public {
+        _burn(tokenId);
+    }
+}
